4f652b40e26df9d8443e371c1096884e62017cdb
wrzrmzx <1510033076@qq.com>
2020-11-01 23:30:01 +0800

    feat: :sparkles: 新手教程通关

--- src/index.js
+++ src/index.js
@@ -18,90 +18,23 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // 每个格子的标记状态，分别为：“X”，“O”，“null（未标记）”
-      squares: Array(9).fill(null),
-      // 判断下一个落子的是“X”还是“O”，利用下一个该落子的是否为“X”进行判定
-      xIsNext: true,
-    };
-  }
-
-  // 每次点击格子的时候调用，将格子标记为“X”或者“O”
-  handleClick(i) {
-    // 该格子已经被标记过或者胜负已分，则直接返回不处理
-    if (this.calculateWinner(this.state.squares) || this.state.squares[i])
-      return;
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
-  }
-
-  // 计算胜者为“X”还是“O”，没有胜者返回null
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      let [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[b] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
   // 将Square作为函数组件，方便复用
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.props.squares[i]}
         onClick={() => {
-          this.handleClick(i);
+          this.props.onClick(i);
         }}
       />
     );
   }
 
-  renderSquare2(i) {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          this.handleClick(i);
-        }}
-      >
-        {this.state.squares[i]}
-      </button>
-    );
-  }
-
   render() {
-    let status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    const winner = this.calculateWinner(this.state.squares);
-    if (winner) {
-      status = "Winner is: " + (this.state.xIsNext ? "O" : "X");
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare2(0)}
+          {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
@@ -121,15 +54,72 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  // 每次点击格子的时候调用，将格子标记为“X”或者“O”
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    // 该格子已经被标记过或者胜负已分，则直接返回不处理
+    if (calculateWinner(squares) || squares[i]) return;
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  // 跳转到第几步
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    // 渲染时光机按钮
+    const moves = history.map((step, move) => {
+      const desc = move ? "go to move #" + move : "go to start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner is: " + (this.state.xIsNext ? "O" : "X");
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -139,3 +129,23 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+// 计算胜者为“X”还是“O”，没有胜者返回null
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    let [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
